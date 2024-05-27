f51c8c59db80d5135cb620d21cbceca4fee6610b
JohndelVillanueva <Johndel0307@gmail.com>
2024-05-27 09:31:46 +0800

    print ui

---
+++ print.php
@@ -0,0 +1,99 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Bootstrap CDN  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="print.css">
+</head>
+
+<body>
+    <!-- base border  -->
+    <div class="container-fluid border border-2 border-black vh-50 p-0 m-0">
+        <!-- first row School logo + QR Code + Event name -->
+        <div class="row col-12 mt-3">
+            <!-- left margin  -->
+            <div style="width:2%"></div>
+            <!-- logo + event name container  -->
+            <div class="col-7 border border-2 border-warning d-flex flex-column justify-content-evenly align-items-center">
+                <div class="mt-4 h-50">
+                    <img src="./assets/logo.PNG" alt="Westfields Logo" class="w-100 h-100">
+                </div>
+                <div class="mb-4 h-50 d-flex align-items-end pb-4">
+                    <p class="h1 fw-medium text-wrap"> Event Name at Event Venue</p>
+                </div>
+            </div>
+            <!-- . margin between  -->
+            <div style="width:14%"></div>
+            <div class="col-3 border border-2 border-black p-0">
+                <div><img src="./images/005_file_664dae93bea94.png" alt="qr" class="w-100 h-100"></div>
+            </div>
+        </div>
+        <br>
+        <div class="row vh-25  p-0 m-0">
+            <!-- left margin  -->
+            <div style="width:2%"></div>
+            <div>
+                <p class="h1 fw-medium text-center border border-2 border-black"> Sample Text for Name Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, nobis?</p>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="row col-12 vh-25 p-0 m-0 ">
+            <!-- left margin  -->
+            <div>
+                <p class="h3 fw-medium text-center border border-2 border-black ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus odit vitae eos itaque, necessitatibus unde tempora suscipit doloribus nesciunt laborum esse dolor. Cupiditate, explicabo corporis inventore nobis nam ab numquam aspernatur enim repellat aperiam dolorum aliquam nostrum praesentium repellendus consectetur sapiente labore qui architecto officia, reprehenderit molestias est doloribus? Doloribus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni recusandae inventore aliquid iste fuga voluptates suscipit laboriosam voluptatum, expedita perspiciatis odit non ab sequi</p>
+            </div>
+        </div>
+        <br>
+    </div>
+    <hr>
+    <!-- base border  -->
+    <div class="container-fluid border border-2 border-black vh-50 p-0 m-0">
+        <!-- first row School logo + QR Code + Event name -->
+        <div class="row col-12 mt-3">
+            <!-- left margin  -->
+            <div style="width:2%"></div>
+            <!-- logo + event name container  -->
+            <div class="col-7 border border-2 border-warning d-flex flex-column justify-content-evenly align-items-center">
+                <div class="mt-4 h-50">
+                    <img src="./assets/logo.PNG" alt="Westfields Logo" class="w-100 h-100">
+                </div>
+                <div class="mb-4 h-50 d-flex align-items-end pb-4">
+                    <p class="h1 fw-medium text-wrap"> Event Name at Event Venue</p>
+                </div>
+            </div>
+            <!-- . margin between  -->
+            <div style="width:14%"></div>
+            <div class="col-3 border border-2 border-black p-0">
+                <div><img src="./images/005_file_664e9bb1e1b03.png" alt="qr" class="w-100 h-100"></div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="row vh-25  p-0 m-0">
+            <!-- left margin  -->
+            <div style="width:2%"></div>
+            <div>
+                <p class="h1 fw-medium text-center border border-2 border-black"> Sample Text for Name Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, nobis?</p>
+            </div>
+        </div>
+        <br>
+        <div class="row col-12 vh-25 p-0 m-0 ">
+            <!-- left margin  -->
+            <div>
+                <p class="h3 fw-medium text-center border border-2 border-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus odit vitae eos itaque, necessitatibus unde tempora suscipit doloribus nesciunt laborum esse dolor. Cupiditate, explicabo corporis inventore nobis nam ab numquam aspernatur enim repellat aperiam dolorum aliquam nostrum praesentium repellendus consectetur sapiente labore qui architecto officia, reprehenderit molestias est doloribus? Doloribus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni recusandae inventore aliquid iste fuga voluptates suscipit laboriosam voluptatum, expedita perspiciatis odit non ab sequi</p>
+            </div>
+        </div>
+        <br>
+    </div>
+</body>
+
+</html>
